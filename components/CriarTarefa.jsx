4a65961a95ba9278315949fb9e61288421678d74
Gabriel Vieira <vieira.front-end@outlook.com>
2022-08-29 23:52:59 -0300

    Initial commit

---
+++ components/CriarTarefa.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function CriarTarefa(props) {
+  const info = {
+    taskName: props.tarefa,
+    taskDeleteKey: `complete@${props.tarefa}`,
+  };
+
+  let [marginTop, setMarginTop] = useState(props.marginTop);
+  let [opacity, setOpacity] = useState(props.opacity);
+  let [height, setHeight] = useState("");
+  let [transform, setTransform] = useState("");
+  let [marginBottom, setMarginBottom] = useState("0px");
+  let [zIndex, setZIndex] = useState("1");
+  let [width, setWidth] = useState("");
+
+  const animationObject = {
+    marginTop,
+    opacity,
+    height,
+    width,
+    transform,
+    marginBottom,
+    zIndex,
+  };
+
+  setTimeout(() => {
+    setMarginTop("");
+    setOpacity(1);
+  }, 100);
+
+  return (
+    <>
+      <div style={animationObject} className={styles.containerTarefa}>
+        <div className={styles.tarefa}>
+          <p>{info.taskName}</p>
+        </div>
+
+        <div className={styles.btnsTarefa} alt={info.taskDeleteKey}>
+          <img
+            src="/confirm.svg"
+            alt={info.taskDeleteKey}
+            className={styles.actionBtn}
+            style={animationObject}
+            onClick={(e) => {
+              setZIndex("0");
+              setTransform("translateY(-35px)");
+              setHeight("0px");
+              setWidth("0vh");
+              setMarginBottom("-20px");
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
